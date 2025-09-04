3b16cbc9d98f0c113bd558796c802f1fb59f247f
Aditya Kumar <Kumaraditya3555@gmail.com>
2025-09-04 11:02:01 +0530

    Display the duration of the music

--- Music_Website.js
+++ Music_Website.js
@@ -1,5 +1,8 @@
 import {musicArray} from './music.js';
 
+let musicIndex=0;
+let audio=new Audio(musicArray[musicIndex].songItem);
+
 const Play=document.querySelector('.Play2');
 const backward=document.querySelector('.backward');
 const forward=document.querySelector('.forward');
@@ -10,11 +13,15 @@ let pauseBtn=document.querySelector('.pause');
 let musicName=document.querySelector('.musicName');
 let musicName2=document.querySelector('.musicName2');
 let progressBar=document.getElementById('myProgressBar');
+let timeIndex=document.querySelector('.timing');
 
-let musicIndex=0;
-let audio=new Audio(musicArray[musicIndex].songItem);
 let isMusicPlay=false;
 
+function showTime(){
+  audio.addEventListener('loadedmetadata',()=>{
+    ShowTiming(audio.duration);
+  });
+}
 
 backward.addEventListener('click',()=>{
   musicIndex=(musicIndex-1+musicArray.length)%musicArray.length;
@@ -28,6 +35,8 @@ backward.addEventListener('click',()=>{
   pauseBtn.classList.add('activeBtn');
   audio.play();
   isMusicPlay=true;
+  showTime();
+  showMe();
 });
 
 Play.addEventListener('click',()=>{
@@ -38,8 +47,8 @@ Play.addEventListener('click',()=>{
     gifImage.classList.remove('active');
     Play.classList.remove('deactiveBtn');
     pauseBtn.classList.remove('activeBtn');
-    musicName.Text=musicArray[musicIndex].songName;
-    musicName2.Text=musicArray[musicIndex].songName;
+    musicName.innerHTML=musicArray[musicIndex].songName;
+    musicName2.innerHTML=musicArray[musicIndex].songName;
   }else{
     audio.play();
     albumCover.src=musicArray[musicIndex].songAlbum;
@@ -49,9 +58,17 @@ Play.addEventListener('click',()=>{
     pauseBtn.classList.add('activeBtn');
     musicName.innerText=musicArray[musicIndex].songName;
     musicName2.innerText=musicArray[musicIndex].songName;
+    isMusicPlay=true;
+    showTime();
+    showMe();
   }
   isMusicPlay=!isMusicPlay;
 });
+
+function showMe(){
+  let durAudio=audio.duration;
+  ShowTiming(durAudio);
+}
 
 forward.addEventListener('click',()=>{
   musicIndex=(musicIndex+1)%musicArray.length;
@@ -65,9 +82,30 @@ forward.addEventListener('click',()=>{
   pauseBtn.classList.add('activeBtn');
   audio.play();
   isMusicPlay=true;
+  showTime();
+  showMe();
 });
 
 audio.addEventListener('timeupdate',()=>{
   let progress=parseInt((audio.currentTime/audio.duration)*100); 
   progressBar.value=progress;
-})
+});
+
+progressBar.addEventListener('input',()=>{
+  audio.currentTime = (progressBar.value/100) * audio.duration;
+});
+
+function ShowTiming(audioDur){
+  let min=Math.floor(audioDur/60);
+  let sec=Math.floor(audioDur%60);
+  console.log(min);
+  if(min<10){
+    if(sec<10){
+      timeIndex.textContent=`0${min}:0${sec}`;
+    }else{
+      timeIndex.textContent=`0${min}:${sec}`;
+    }
+  }else{
+    timeIndex.textContent=`${min}:${sec}`;
+  }
+}  
