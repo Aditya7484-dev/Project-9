461e94ef3dd3fb64e7b5a81094ea348992ae1c4f
Aditya Kumar <Kumaraditya3555@gmail.com>
2026-05-02 14:07:10 +0530

    Added html for play and pause buttons

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 // server/server.js
 const express = require("express");
+const cors = require("cors");
 
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -7,6 +8,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
 app.get("/musicApp", (req, res) => {
